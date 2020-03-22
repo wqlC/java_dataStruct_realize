853cfeaa6712b8fb4ee3b0a6cf44637eb8691558
tailor <tailor@nwpu.com>
2020-03-22 19:01:08 +0800

    add test program to ArrayQueue and LoopQueue

---
+++ Queue/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+/**
+ * @author tailor
+ * @create 2020/3/22 - 18:34
+ * @mail wql2014302721@gmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue time: " + time1+ "s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue time: " + time2+ "s");
+
+    }
+
+    public static double testQueue(Queue<Integer> queue, int opCount){
+        double time1 = System.nanoTime();
+        Random random = new Random();
+        for(int i=0; i<opCount; i++){
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for(int i=0; i<opCount; i++){
+            queue.dequeue();
+        }
+
+        double time2 = System.nanoTime();
+        return (time2-time1)/1000000000.0;
+    }
+}
